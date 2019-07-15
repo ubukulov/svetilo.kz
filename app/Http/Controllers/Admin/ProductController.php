f51e65042a983bab61168c6a2c55c63a37ff966b
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-15 10:26:30 +0600

    fix

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -161,6 +161,7 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Product::destroy($id);
+        return redirect()->route('admin.product.index');
     }
 }
