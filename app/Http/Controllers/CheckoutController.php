3f5289ba63af371ec870248dfae4450c5c329398
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-15 13:14:35 +0600

    fix

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -19,7 +19,7 @@ class CheckoutController extends BaseController
 
     public function store(Request $request)
     {
-        //dd($request->all());
+        $data = $request->all();
         $email = $request->input('email');
         $cartItems = ShoppingCart::getCartItems();
         if (\Auth::check() || $exists_user = User::exists($email)) {
@@ -37,7 +37,7 @@ class CheckoutController extends BaseController
             $order_details = $request->only(['phone', 'address', 'order_notes']);
             $order_details['user_id'] = $user->id;
 
-            DB::transaction(function () use ($order_details, $cartItems) {
+            DB::transaction(function () use ($order_details, $cartItems, $data) {
                 $order_id = Order::create($order_details)->id;
 
                 foreach($cartItems as $cartItem) {
@@ -46,7 +46,7 @@ class CheckoutController extends BaseController
                         'quantity' => $cartItem->quantity, 'price' => $cartItem->product->getPrice()
                     ]);
                 }
-
+                $this->sendToMail($data, $cartItems);
                 ShoppingCart::clearCart();
             });
 
@@ -69,10 +69,64 @@ class CheckoutController extends BaseController
                         'quantity' => $cartItem->quantity, 'price' => $cartItem->product->getPrice()
                     ]);
                 }
-
+                $this->sendToMail($data, $cartItems);
                 ShoppingCart::clearCart();
             });
             return back()->with('message', 'Заказ успешно принят. В ближающее время вам позвонить оператор.');
         }
+    }
+
+    public function sendToMail($data, $cartItems)
+    {
+        $first_name = $data['first_name'];
+        $phone_number = $data['phone'];
+        $email = $data['email'];
+        $to = "Ibraemovm@gmail.com" . ", ";
+        $to .= "kairat_ubukulov@mail.ru";
+        $subject = "Новый заказ";
+        $message = "
+        <html>
+        <head>
+         <title>Новый заказ</title>
+        </head>
+        <body>
+        <p>Имя: $first_name</p>
+        <p>Телефон: $phone_number</p>
+        <p>Email: $email</p>
+        <table>
+            <thead>
+                <th>Наименование</th>
+                <th>Количество</th>
+                <th>Цена</th>
+                <th>Общая сумма</th>
+            </thead>
+            <tbody>
+        ";
+        foreach($cartItems as $cartItem) {
+            $title = $cartItem->product->title;
+            $qty = $cartItem->quantity;
+            $price = $cartItem->product->getPrice();
+            $sum = $qty * $price;
+            $message .= "<tr>";
+            $message .= "<td>$title</td>";
+            $message .= "<td>$qty</td>";
+            $message .= "<td>$price</td>";
+            $message .= "<td>$sum</td>";
+            $message .= "</tr>";
+        }
+        $message .=
+        "    
+            </tbody>
+        </table>
+        </body>
+        </html>
+        ";
+        /* Для отправки HTML-почты вы можете установить шапку Content-type. */
+        $headers= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=utf-8\r\n";
+        /* дополнительные шапки */
+        $headers .= "From: Новый заказ <zakaz@svetilo.kz>\r\n";
+//        $headers .= "Cc: birthdayarchive@example.com\r\n";
+        mail($to, $subject, $message, $headers);
     }
 }
