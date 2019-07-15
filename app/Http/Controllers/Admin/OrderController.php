3f3159f7ca8dcfb378b3a0355e99a717d4e24573
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-15 15:06:34 +0600

    fix

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Order::orderBy('id', 'desc')->get();
+        return view('admin.order.index', compact('orders'));
+    }
 }
