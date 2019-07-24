2ece751a5dc21fd5285a370062167fb65de7f2c5
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-24 16:41:36 +0600

    fix

--- resources/views/themes/besmart/layouts/app.blade.php
+++ resources/views/themes/besmart/layouts/app.blade.php
@@ -126,5 +126,25 @@
     </div>
 </div>
 <script src="//code.jivosite.com/widget.js" jv-id="1QZp71yzLz" async></script>
+<!-- WhatsHelp.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+77777941654", // WhatsApp number
+            call: "+77777941654", // Call phone number
+            company_logo_url: "//static.whatshelp.io/img/flag.png", // URL of company logo (png, jpg, gif)
+            greeting_message: "Здравствуйте! Отправьте нам сообщение через любой из мессенджеров.", // Text of greeting message
+            call_to_action: "Напишите нам", // Call to action
+            button_color: "#A8CE50", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "whatsapp,call", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /WhatsHelp.io widget -->
 </body>
 </html>
