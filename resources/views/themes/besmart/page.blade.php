df38314e050a9ac9636dde78e37ccb4133fa1a8f
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-07-24 16:26:06 +0600

    add yandex map for page contacts

--- resources/views/themes/besmart/page.blade.php
+++ resources/views/themes/besmart/page.blade.php
@@ -1,4 +1,8 @@
 @extends('themes.besmart.layouts.app')
 @section('content')
     {!! $page->full_description !!}
+
+    @if($page->alias == 'kontakty')
+        <iframe src="https://yandex.kz/map-widget/v1/-/CChuj2iX" width="560" height="400" frameborder="1" allowfullscreen="true"></iframe>
+    @endif
 @stop
